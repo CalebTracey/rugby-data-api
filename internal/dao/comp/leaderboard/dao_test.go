c0435e896a38ececfc2ed4b2a056af5776d11343
Caleb Tracey <caleb.tracey@gmail.com>
2022-12-04 13:43:40 -0500

    added coverage

--- internal/dao/comp/leaderboard/dao_test.go
+++ internal/dao/comp/leaderboard/dao_test.go
@@ -114,3 +114,87 @@ func TestDAO_GetLeaderboardData(t *testing.T) {
 		})
 	}
 }
+
+func TestDAO_AllLeaderboardData(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockDao := dbmocks.NewMockDAOI(ctrl)
+	mockMapper := dbmocks.NewMockMapperI(ctrl)
+	mockRows := sqlmock.NewRows([]string{"comp_id", "comp_name", "team_id", "team_name"}).
+		AddRow(123, "Test Comp", 1, "Team 1").
+		AddRow(123, "Test Comp", 2, "Team 2")
+	type fields struct {
+		DbDAO    psql.DAOI
+		DbMapper psql.MapperI
+	}
+	type args struct {
+		ctx context.Context
+	}
+	tests := []struct {
+		name          string
+		fields        fields
+		args          args
+		mockRows      *sqlmock.Rows
+		wantResp      models.PSQLLeaderboardDataList
+		wantErr       error
+		mockErr       error
+		wantMapErr    error
+		expectDbError bool
+	}{
+		{
+			name: "Happy Path",
+			fields: fields{
+				DbDAO:    mockDao,
+				DbMapper: mockMapper,
+			},
+			args: args{
+				ctx: context.Background(),
+			},
+			mockRows: mockRows,
+			wantResp: models.PSQLLeaderboardDataList{
+				{
+					CompId:   123,
+					CompName: "Test Comp",
+					TeamId:   1,
+					TeamName: "Team 1",
+				},
+				{
+					CompId:   123,
+					CompName: "Test Comp",
+					TeamId:   2,
+					TeamName: "Team 2",
+				},
+			},
+			mockErr:       nil,
+			wantErr:       nil,
+			wantMapErr:    nil,
+			expectDbError: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := DAO{
+				DbDAO:    tt.fields.DbDAO,
+				DbMapper: tt.fields.DbMapper,
+			}
+			mockDao.EXPECT().FindAll(tt.args.ctx, psql.AllLeaderboardsQuery).
+				DoAndReturn(func(ctx context.Context, query string) (*sqlmock.Rows, error) {
+					if tt.expectDbError {
+						return tt.mockRows, tt.wantErr
+					}
+					return tt.mockRows, nil
+				})
+			if !tt.expectDbError {
+				mockMapper.EXPECT().RowsToLeaderboardData(gomock.Any()).Return(tt.wantResp, tt.wantMapErr)
+			}
+			gotResp, err := s.AllLeaderboardData(tt.args.ctx)
+			if err != tt.wantErr {
+				t.Errorf("AllLeaderboardData() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotResp, tt.wantResp) {
+				t.Errorf("AllLeaderboardData() gotResp = %v, want %v", gotResp, tt.wantResp)
+			}
+		})
+	}
+}
